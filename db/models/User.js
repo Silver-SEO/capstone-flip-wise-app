9f1dc239f6b268e4c9c4c9e60a4536c2fbd63a13
Herr <g.kerber@posteo.it>
2025-02-28 15:43:13 +0100

    adjust datamodel for user

--- db/models/User.js
+++ db/models/User.js
@@ -3,6 +3,14 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
   userIdProvider: { type: String, required: true },
+
+  title: { type: String },
+  image: {
+    width: { type: String, required: true },
+    height: { type: String, required: true },
+    url: { type: String, required: true },
+  },
+  content: { type: String },
 });
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
