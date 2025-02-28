3db459c923ff6c075bb3af59dfcf1dd8a6a07623
Herr <g.kerber@posteo.it>
2025-02-28 15:06:51 +0100

    restore index.js with state of main

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import styled from "styled-components";
-import ImageUpload from "@/components/ImageUpload";
-import Image from "next/image";
 import Link from "next/link";
 
 const ModeList = styled.ul`
@@ -27,39 +25,93 @@ const ModeItem = styled.li`
   }
 `;
 
-const StyledImage = styled(Image)`
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
+const BaseLink = styled(Link)`
+  text-decoration: none;
+  color: ${({ theme }) => theme.collectionCardText};
 `;
 
-export default function Homepage({
-  flashcards,
-  collections,
-  handleCreateCollection,
-  handleDeleteCollection,
-  handleUpdateCollection,
-}) {
-  const image = { title: "Bildtitel", url: "/asset/info.png" };
+const StyledCardLink = styled(BaseLink)`
+  height: 100%;
+  position: relative;
+`;
+
+const ModeArticle = styled.article`
+  height: 100%;
+  background: ${({ theme }) => theme.collectionCard};
+  padding: 0 24px;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+
+const StyledTitle = styled.h3`
+  font-size: 2.2rem;
+  font-weight: 700;
+  margin-bottom: 16px;
+  margin-top: 16px;
+
+  @media (max-width: 768px) {
+    font-size: 1.8rem;
+  }
+`;
+
+const StyledStatsItem = styled.p`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  font-size: 1.3rem;
+  margin-top: 4px;
+  @media (max-width: 768px) {
+    font-size: 1.1rem;
+    margin-bottom: 54px;
+  }
+`;
+const StyledStatsItemSpan = styled.span`
+  font-weight: 700;
+  background: ${({ theme }) => theme.collectionCardText};
+  color: ${({ theme }) => theme.collectionCard};
+  padding: 4px 8px;
+  border-radius: 8px;
+`;
+
+export default function Homepage({}) {
   return (
     <>
-      <Container>
-        <StyledPageTitle>List of collections</StyledPageTitle>
-      </Container>
-      <ImageUpload></ImageUpload>
-      {/*       <StyledImage
-        alt={`image of ${image.title}`}
-        src={image.url}
-        fill
-        style={{ objectFit: "contain" }}
-      ></StyledImage> */}
-      <CollectionList
-        flashcards={flashcards}
-        collections={collections}
-        handleCreateCollection={handleCreateCollection}
-        handleDeleteCollection={handleDeleteCollection}
-        handleUpdateCollection={handleUpdateCollection}
-      />
+      <ModeList>
+        <ModeItem>
+          <StyledCardLink href="/collections">
+            <ModeArticle>
+              <StyledTitle>Learning Mode</StyledTitle>
+              <StyledStatsItem>
+                Description:
+                <StyledStatsItemSpan>
+                  This is the Learning Mode. Here you can find a list of
+                  collections. In each collection you can find a list of
+                  flashcards with questions and answers to the topic of the
+                  collection. Also you can create new cards and collections,
+                  edit and delete existing ones.
+                </StyledStatsItemSpan>
+              </StyledStatsItem>
+            </ModeArticle>
+          </StyledCardLink>
+        </ModeItem>
+        <ModeItem>
+          <StyledCardLink href="/quiz">
+            <ModeArticle>
+              <StyledTitle>Quiz Mode</StyledTitle>
+              <StyledStatsItem>
+                Description:
+                <StyledStatsItemSpan>
+                  Test your knowledge in Quiz Mode! Select a collection and
+                  difficulty level to start. Challenge yourself with different
+                  time limits and card quantities. If needed, AI will generate
+                  additional cards to ensure a complete quiz experience.
+                </StyledStatsItemSpan>
+              </StyledStatsItem>
+            </ModeArticle>
+          </StyledCardLink>
+        </ModeItem>
+      </ModeList>
     </>
   );
 }
